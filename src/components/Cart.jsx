a2a3f458a5dc545c1d962c90349902170a9f2262
Benkku <61476044+Bertrand14@users.noreply.github.com>
2024-12-02 09:50:32 +0200

    Delete src directory

--- src/components/Cart.jsx
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react'
-import presentation from './../assets/data/piafslife'
-
-export default function Cart(){
-
-}
--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import './../styles/_variables.scss'
-
-export default function FooterPage(){
- return (
-  <footer className='footerPage'>
-  </footer>
- )
-}
